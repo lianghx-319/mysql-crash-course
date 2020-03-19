afe9b02d31fc6941910e39de23c2e694a3c7e1f6
Han <xsytby1112@gmail.com>
2020-03-19 22:48:53 +0800

    feat: use docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
 # Derived from official mysql image (our base image)
 FROM mysql
-# Add a database
-ENV MYSQL_DATABASE crash
 # Add the content of the sql-scripts/ directory to your image
 # All scripts in docker-entrypoint-initdb.d/ are automatically
 # executed during container startup
 COPY ./sql-scripts/ /docker-entrypoint-initdb.d/
-
-VOLUME /opt/data/mysql:/var/lib/mysql
-
-EXPOSE 3306
---
+++ start.sh
@@ -0,0 +1,18 @@
+DB_FOLDER="./db_data"
+DB_PASSWORD_FILE="./db_password.txt"
+DB_ROOT_PASSWORD_FILE="./db_root_password.txt"
+INIT_PASSWORD="secretpassword"
+
+if [ ! -d "$DB_FOLDER" ]; then
+  mkdir -p $DB_FOLDER
+fi
+
+if [ ! -f "$DB_PASSWORD_FILE" ]; then
+  touch "$DB_PASSWORD_FILE" && echo $INIT_PASSWORD > $DB_PASSWORD_FILE
+fi
+
+if [ ! -f "$DB_ROOT_PASSWORD_FILE" ]; then
+  touch "$DB_ROOT_PASSWORD_FILE" && echo $INIT_PASSWORD > $DB_ROOT_PASSWORD_FILE
+fi
+
+docker-compose up -d 
